1cd948c5440614d41328e4b26fea44f47af33218
Adam Ali <adamka2000@outlook.com>
2024-07-08 18:03:26 +0100

    Fix marking message as deleted

--- src/message/message.data.ts
+++ src/message/message.data.ts
@@ -88,8 +88,18 @@ export class MessageData {
   }
 
   async delete(messageId: ObjectID): Promise<ChatMessage> {
-    // TODO allow a message to be marked as deleted
-    return new ChatMessage() // Minimum to pass ts checks -replace this
+    const filterBy = { _id: messageId };
+    const updateProperty = { deleted: true };
+    const result = await this.chatMessageModel.findOneAndUpdate(
+      filterBy,
+      updateProperty,
+      {
+        new: true,
+        returnOriginal: false,
+      },
+    );
+    if (!result) throw new Error('Message to be deleted not found');
+    return chatMessageToObject(result); // Minimum to pass ts checks -replace this
   }
 
   async resolve(messageId: ObjectID): Promise<ChatMessage> {
